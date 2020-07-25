c320455eca5ffb07477fea0cdcf0f143ffd66026
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-07-25 12:04:52 -0700

    Added dram model.

--- drams/models.py
+++ drams/models.py
@@ -5,4 +5,24 @@ from django.contrib.auth.models import User
 # Model for individual whisk(e)y reviews
 class Dram(models.Model):
 
-    pass
+    # Define different categories of whisk(e)y
+    CATEGORIES = (
+        ("SM", "Single Malt Scotch"),
+        ("BS", "Blended Scotch"),
+        ("BMS", "Blended Malt Scotch"),
+        ("IB", "Irish Blend"),
+        ("SPS", "Single Pot Still Irish"),
+        ("B", "Bourbon"),
+        ("CW", "Canadian Whisky"),
+        ("RW", "Rye Whiskey"),
+        ("JW", "Japanese Whisky"),
+        ("WW", "World/Misc Whisk(e)y"),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dram = models.CharField(max_length=250)
+    company = models.CharField(max_length=250)
+    category = models.CharField(max_length=30, choices=CATEGORIES, default="WW")
+    abv = models.DecimalField(max_digits=4, decimal_places=2)
+    nosing = models.TextField(default="Add nosing notes here.")
+    tasting = models.TextField(default="Add tasting notes here.")
